54224ee4653b98354c30a9fcf60e4171a31a681e
Binod Raj Dhami <binodraazdhami@gmail.com>
2025-09-28 18:54:20 +0545

    Control Flow: If, else, elif

---
+++ 8_control_flow.py
@@ -0,0 +1,42 @@
+# if
+# else
+# else if
+
+# match
+# Loops: while, do while, for
+
+ram_age = 18
+eligible_for_vote = 18
+
+if (ram_age >= eligible_for_vote):
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+my_age = 20
+eligible_age = 18
+is_employee = False
+
+if (my_age >= eligible_age and is_employee == True):
+    print("Eligible for Credit Card")
+else:
+    print("Not eligible for Credit Card")
+
+math = 60
+english = 55
+
+if (math >= 60 or english >= 60):
+    print("Eligible for study Science +2")
+else:
+    print("Not eligible for study in science")
+
+
+is_raining = False
+is_windy = True
+
+if (is_raining == True and is_windy == True):
+    print("I will carry Rain Coat")
+elif (is_raining == True and is_windy == False):
+    print("I will carry Umbrella")
+else:
+    print("I will not carry umbrella")
