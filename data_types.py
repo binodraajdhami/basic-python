356643d79ee19f6ebf659c2340060b40f81c4630
Binod Raj Dhami <binodraazdhami@gmail.com>
2025-12-09 22:34:12 +0545

    I learn data tyepes

---
+++ data_types.py
@@ -0,0 +1,11 @@
+name = "Ram Thapa"
+address = 'Hattiban'
+phone = 98888888
+email = "ram@gmail.com"
+status = True
+
+print(name)
+print(address)
+print(phone)
+print(email)
+print("Married Status : ", status)
