d25686705ff314558e070f7bb4e5310d661b9c0a
Binod Raj Dhami <binodraazdhami@gmail.com>
2025-12-10 22:48:36 +0545

    Variables

---
+++ day_2_variables.py
@@ -0,0 +1,15 @@
+email = "ram@gmail.com"
+name1 = "Ram"
+name2 = "Sita"
+name3 = "Gita"
+
+first_name = "Rahul"
+last_name = "Rai"
+
+
+print(email)
+print(name1)
+print(name2)
+print(name3)
+print(first_name)
+print(last_name)
