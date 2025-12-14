2d63881fe83bff1d60eb25537724a149c922cae5
Binod Raj Dhami <binodraazdhami@gmail.com>
2025-12-14 22:35:31 +0545

    Operators

---
+++ day_4_comparison.py
@@ -0,0 +1,26 @@
+num1 = 20
+num2 = 20
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 <= num2)
+print(num1 >= num2)
+
+voting = 18
+ram_age = 20
+if (ram_age >= voting):
+    print("Can Vote")
+else:
+    print("Can't Vote")
+
+
+min_age = 18
+max_age = 60
+hari_age = 61
+
+if (hari_age >= min_age and hari_age <= max_age):
+    print("Can get loan")
+else:
+    print("Can't get loan")
---
+++ day_4_number.py
@@ -0,0 +1,33 @@
+num1 = 10
+num2 = 20
+
+sum_of_two_number = num1 + num2
+sub_of_two_number = num1 - num2
+print(sub_of_two_number)
+
+print(3*3)
+print(10/3)
+print(10 % 3)
+print(10 // 3)
+print(10 ** 5)
+print(10*10*10*10*10)
+
+x = 5
+x = x + x + 3
+
+y = 5
+y += 3
+
+print(x)
+print(y)
+
+
+name = "Ram"
+name = name + "Hari"
+print(name)
+
+
+a = 10
+a &= 3
+
+print(a)
